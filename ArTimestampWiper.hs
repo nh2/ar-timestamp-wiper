c03e28c8eb71356b385f85e75d7f4d1927f19496
Niklas Hambüchen <mail@nh2.me>
2013-08-07 10:33:30 +0900

    Check for file existence before running

--- ArTimestampWiper.hs
+++ ArTimestampWiper.hs
@@ -7,49 +7,56 @@ import           Control.Monad
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BS8
 import           Data.Char
+import           System.Directory
 import           System.IO
 
 
 -- | Removes the time stamps of all files in the .a file.
 arFileWipeTimeStamps :: FilePath -> IO ()
-arFileWipeTimeStamps path = withBinaryFile path ReadWriteMode $ \h -> do
+arFileWipeTimeStamps path = do
 
-  -- We iterate through the archive stepping from one file header to the next,
-  -- setting the time stamp field to zero.
-  -- The size field tells us where the next header is.
-  -- See: http://en.wikipedia.org/wiki/Ar_%28Unix%29.
+  -- Check for file existence (ReadWriteMode would create one otherwise)
+  exsists <- doesFileExist path
+  when (not exsists) $ error $ "arFileWipeTimeStamps: No such file: " ++ path
 
-  archiveSize <- hFileSize h
+  withBinaryFile path ReadWriteMode $ \h -> do
 
-  let go entryOffset | entryOffset == archiveSize = return () -- done, at end
-                     | entryOffset >  archiveSize = die "Archive truncated"
-                     -- Headers are aligned to even bytes
-                     | odd entryOffset            = go (entryOffset + 1)
-                     | otherwise = do
+    -- We iterate through the archive stepping from one file header to the next,
+    -- setting the time stamp field to zero.
+    -- The size field tells us where the next header is.
+    -- See: http://en.wikipedia.org/wiki/Ar_%28Unix%29.
 
-        -- Sanity check
-        magic <- goto 58 >> BS.hGet h 2
-        when (magic /= "\x60\x0a") $ die "Bad ar magic"
+    archiveSize <- hFileSize h
 
-        -- Get size (to find following file)
-        size <- goto 48 >> parseSize . BS8.unpack <$> BS.hGet h 10
+    let go entryOffset | entryOffset == archiveSize = return () -- done, at end
+                       | entryOffset >  archiveSize = die "Archive truncated"
+                       -- Headers are aligned to even bytes
+                       | odd entryOffset            = go (entryOffset + 1)
+                       | otherwise = do
 
-        -- Wipe time stamp
-        goto 16 >> BS.hPut h "0           " -- 12 chars
+          -- Sanity check
+          magic <- goto 58 >> BS.hGet h 2
+          when (magic /= "\x60\x0a") $ die "Bad ar magic"
 
-        -- Seek to next file at header + file size
-        go (entryOffset + 60 + size)
+          -- Get size (to find following file)
+          size <- goto 48 >> parseSize . BS8.unpack <$> BS.hGet h 10
 
-        where
-          goto pos = hSeek h AbsoluteSeek (entryOffset + pos)
+          -- Wipe time stamp
+          goto 16 >> BS.hPut h "0           " -- 12 chars
 
-          parseSize x = case reads x of
-            [(s, r)] | all isSpace r -> s
-            _                        -> die "Malformed header"
+          -- Seek to next file at header + file size
+          go (entryOffset + 60 + size)
 
-          die msg = error $ "arFileWipeTimeStamps: " ++ path ++ ": "
-                            ++ msg ++ " at offset " ++ show entryOffset
+          where
+            goto pos = hSeek h AbsoluteSeek (entryOffset + pos)
 
-  go 8 -- 8 == size of global header, before first file header
+            parseSize x = case reads x of
+              [(s, r)] | all isSpace r -> s
+              _                        -> die "Malformed header"
+
+            die msg = error $ "arFileWipeTimeStamps: " ++ path ++ ": "
+                              ++ msg ++ " at offset " ++ show entryOffset
+
+    go 8 -- 8 == size of global header, before first file header
 
 
